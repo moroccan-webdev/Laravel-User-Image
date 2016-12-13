161b2511ee90cf64bdb5097a73656f4e7eebc882
Web Dev <moroccanwebdeveloper@gmail.com>
2016-12-13 11:07:17 +0000

    Install font-awesome and filp.whoops debbuging error

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,6 +12,7 @@
 
     <!-- Styles -->
     <link href="/css/app.css" rel="stylesheet">
+    <link href="/css/font-awesome.css" rel="stylesheet">
     <link href="/css/style.css" rel="stylesheet">
 
     <!-- Scripts -->
